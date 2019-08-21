e482c65f5e74df12770196f13dc14a95f39753d3
Katie Rose <katiefelten@gmail.com>
2019-08-21 17:04:46 -0400

    :zap: 200 res on GET /

---
+++ auth/auth-router.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+router.post("/register", (req, res) => {});
+
+router.post("/login", (req, res) => {});
+
+module.exports = router;
