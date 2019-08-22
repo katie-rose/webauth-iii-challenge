8db745751ba5944d1dae7157b6cbf8ff1de490d5
Katie Rose <katiefelten@gmail.com>
2019-08-22 17:24:07 -0400

    :zap: successfully created a login and token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,7 +7,7 @@ const secrets = require("../config/secrets.js");
 
 router.post("/register", (req, res) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  const hash = bcrypt.hashSync(user.password, 10); 
   user.password = hash;
 
   Users.add(user)
